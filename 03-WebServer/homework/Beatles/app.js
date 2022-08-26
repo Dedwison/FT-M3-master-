bc1324a36207820de2dbc36be3c97448185bc464
Dedwison <dedwison@gmail.com>
2022-08-26 19:42:14 -0400

    resolucion ejercicios 03-WebServer

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -1,24 +1,28 @@
-var http = require('http');
-var fs   = require('fs');
+var http = require("http");
+const { home, api, artista, perfil, beatles } = require("./controlador");
 
-var beatles=[{
-  name: "John Lennon",
-  birthdate: "09/10/1940",
-  profilePic:"https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
-},
-{
-  name: "Paul McCartney",
-  birthdate: "18/06/1942",
-  profilePic:"http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg"
-},
-{
-  name: "George Harrison",
-  birthdate: "25/02/1946",
-  profilePic:"https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
-},
-{
-  name: "Richard Starkey",
-  birthdate: "07/08/1940",
-  profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
-}
-]
+const rutas = {
+  "/": home,
+  "/api": api,
+  "/api/": api,
+};
+http
+  .createServer(function (req, res) {
+    if (rutas[req.url]) {
+      rutas[req.url](req, res);
+    } else {
+      if (req.url.substring(0, 5) === "/api/") {
+        artista(req, res);
+      } else {
+        if (req.url.includes("20")) {
+          perfil(req, res);
+        } else {
+          res.writeHead(404);
+          res.end("No encontramos la ruta solicitada");
+        }
+      }
+    }
+  })
+  .listen(3001, () => {
+    console.log(`Running on PORT: 3000}`);
+  });
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,32 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs");
+var http = require("http");
 
 // Escribí acá tu servidor
+
+http
+  .createServer(function (req, res) {
+    console.log(req.url);
+    if (req.url === "/") {
+      res.writeHead(201, { "Content-Type": "Aplication/json" });
+
+      const obj = {
+        msj: "Intenta buscar los siguientes nombres de Doges",
+        names: ["code", "badboy", "retrato", "resaca", "sexy", "arcoiris"],
+      };
+
+      res.end(JSON.stringify(obj));
+    } else {
+      fs.readFile(`./images${req.url}_doge.jpg`, (err, LecturaImg) => {
+        if (err) {
+          res.writeHead(404);
+          res.end(`Imagen ${req.url} no encontrada`);
+        } else {
+          res.writeHead(200);
+          res.end(LecturaImg);
+        }
+      });
+    }
+  })
+  .listen(3000, () => {
+    console.log("running on PORT 3000");
+  });
