eb7a18b237d0b5d46564262e10297286a43d4bf7
Dedwison <dedwison@gmail.com>
2022-08-29 20:11:43 -0400

    resolucion ejercicio 07-FunctionGenerator

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,18 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max) {
   // Tu código acá:
-
+  let number = 1;
+  while (max ? number <= max : true) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      yield "Fizz Buzz";
+    } else if (number % 3 === 0) {
+      yield "Fizz";
+    } else if (number % 5 === 0) {
+      yield "Buzz";
+    } else {
+      yield number;
+    }
+    number++;
+  }
 }
 
 module.exports = fizzBuzzGenerator;
